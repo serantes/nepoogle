102f4746c7265e3916b589ed38da47d9aa908324
Ignacio Serantes <kde@aynoa.net>
2013-04-24 13:40:36 +0200

    FIX: urlHtmlEncode raise an error for empty urls.

--- lfunctions.py
+++ lfunctions.py
@@ -436,6 +436,9 @@ def urlDecode(url, retryIfThereArePercentCharacters = True):
 
 
 def urlHtmlEncode(url):
+    if not url:
+        return url
+    
     if ((url[0] == "/") or (url[:7] == "file://")):
         return url.replace("\"", "&quot;").replace("#", "%23").replace("'", "&#39;").replace("?", "%3F")
 
