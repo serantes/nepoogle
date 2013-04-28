4fe088422b8bbd4ba7a99990177df4cc93756435
Ignacio Serantes <kde@aynoa.net>
2013-04-28 21:45:17 +0200

    NEW: icon for SPARQL queries.

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ BINNAME=$PROGRAMNAME
 DESKTOPDIR=~/.local/share/applications
 ICONSDIR=~/.local/share/icons/hicolor
 ICONS32="rating_empty.png rating_full.png rating_half.png"
-ICONS48="orientation_1.png orientation_2.png orientation_3.png orientation_4.png orientation_5.png orientation_6.png orientation_7.png orientation_8.png"
+ICONS48="orientation_1.png orientation_2.png orientation_3.png orientation_4.png orientation_5.png orientation_6.png orientation_7.png orientation_8.png sparql.png"
 ICONS128="no_cover.png no_photo.png no_symbol.png no_video_thumbnail.png"
 
 if ! [[ -d "$BINDIR" ]]; then
